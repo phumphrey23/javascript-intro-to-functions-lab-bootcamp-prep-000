28da6c87cd2ece1103344fd59b99bb851acb175d
Precious Humphrey <phumphrey23@yahoo.com>
2017-11-04 18:30:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(hello){
+  console.log('HELLO')
+}
+
+function logWhisper(HELLO){
+  console.log('hello')
+}
