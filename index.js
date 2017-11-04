eb21cc530cae8e17bc2e613e23c4d05843837f21
Precious Humphrey <phumphrey23@yahoo.com>
2017-11-04 18:46:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(HELLO){
   console.log('hello')
 }
 
-function sayHiToGrandma(if (string.toLowerCase() === string)){
+function sayHiToGrandma(string){
+  if (string.toLowerCase() === 'string')
   return 'I can\'t hear you'
 }
