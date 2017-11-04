8d6dd7da2074434ec6a0d358b02bb273743be68b
Precious Humphrey <phumphrey23@yahoo.com>
2017-11-04 18:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,9 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase())
   return 'I can\'t hear you!';
 }
+  {else if (string === string.toUpperCase())
+  return 'YES INDEED!'
+}
+  {else if (string === 'I love you, Grandma.')
+  return 'I love you, too.'
+}
